89e3fa851f2b58cb2b7265944e0859b34450ce56
bm <pyjedi@gmail.com>
2013-12-07 22:32:48 -0500

    Add general, includes git config

---
+++ install_general.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -e
+
+# git config stuff
+git config --global core.excludesfile $HOME/.gitignore_global
+git config --global user.name bm
+git config --global user.email pyjedi@gmail.com

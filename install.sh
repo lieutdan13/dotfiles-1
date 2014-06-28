520a0ecef8c424f360a11104e8016fb24b3aaaaf
bmoar <pyjedi@gmail.com>
2014-06-28 18:32:54 -0400

    Remove old junk

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-repo_path="$HOME/github/desktop"
-cd $repo_path
-
-/bin/sh install_dotfiles.sh
-/bin/sh install_bin.sh
-/bin/sh install_i3.sh
--- install_general.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-# git config stuff
-git config --global core.excludesfile $HOME/.gitignore_global
-git config --global user.name bm
-git config --global user.email pyjedi@gmail.com
